42dd9c3042fb3297db195244a2790817d5bc9629
kuba <kuba.jazdzyk@gmail.com>
2025-04-08 13:57:34 +0100

    sidebar

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -7,23 +7,23 @@ export const Footer = () => {
         <footer className='hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2'>
             <div className='max-w-screen-lg mx-auto flex items-center justify-evenly h-full'>
                 <Button size={"lg"} variant={"ghost"}>
-                    <Image src={"/green_body_squircle.png"} alt='Croatian' height={32} width={40} className='mr-4 rounded-md' />
+                    <Image src={"/green.png"} alt='Croatian' height={32} width={40} className='mr-4 rounded-md' />
                     Croatian
                 </Button>
                 <Button size={"lg"} variant={"ghost"}>
-                    <Image src={"/green_body_squircle.png"} alt='Spanish' height={32} width={40} className='mr-4 rounded-md' />
+                    <Image src={"/green.png"} alt='Spanish' height={32} width={40} className='mr-4 rounded-md' />
                     Spanish
                 </Button>
                 <Button size={"lg"} variant={"ghost"}>
-                    <Image src={"/green_body_squircle.png"} alt='French' height={32} width={40} className='mr-4 rounded-md' />
+                    <Image src={"/green.png"} alt='French' height={32} width={40} className='mr-4 rounded-md' />
                     French
                 </Button>
                 <Button size={"lg"} variant={"ghost"}>
-                    <Image src={"/green_body_squircle.png"} alt='Italian' height={32} width={40} className='mr-4 rounded-md' />
+                    <Image src={"/green.png"} alt='Italian' height={32} width={40} className='mr-4 rounded-md' />
                     Italian
                 </Button>
                 <Button size={"lg"} variant={"ghost"}>
-                    <Image src={"/green_body_squircle.png"} alt='Japanese' height={32} width={40} className='mr-4 rounded-md' />
+                    <Image src={"/green.png"} alt='Japanese' height={32} width={40} className='mr-4 rounded-md' />
                     Japanese
                 </Button>
             </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,34 @@
-type Props = {
-    className?
-}
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
 
-export const Sidebar = () => {
+type Props = {
+    className?: string;
+};
+
+export const Sidebar = ({ className }: Props) => {
     return (
-        <div className="flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col">
-            Sidebar
+        <div className={cn(
+            "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+            className)}>
+            <Link href={"/learn"}>
+                <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+                    <Image src={"/green.png"} height={40} width={40} alt='GreenBody' />
+                    <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+                        ChessEZ
+                    </h1>
+                </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem label="learn" href="/learn" iconSrc={"/green.png"} />
+                <SidebarItem label="leaderbord" href="/leaderboard" iconSrc={"/green.png"} />
+                <SidebarItem label="quests" href="/quests" iconSrc={"/green.png"} />
+                <SidebarItem label="shop" href="/shop" iconSrc={"/green.png"} />
+            </div>
+            <div className="p-4">
+                <SidebarItem label="shop" href="/shop" iconSrc={"/green.png"} />
+            </div>
         </div>
     );
 };
